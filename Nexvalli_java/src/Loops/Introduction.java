a8feb9151a984f3f64a86c443e6c65fd65462202
kiruba-code <talk2kiruba@gmail.com>
2021-10-28 00:02:36 +0530

    update

---
+++ Nexvalli_java/src/Loops/Introduction.java
@@ -0,0 +1,21 @@
+package Loops;
+
+public class Introduction {
+    //Execute a block of statements repeatedly until a termination
+	//upto a contion is met
+	
+	/*
+	 * Repeated over and over untila specfic goal is reached
+	 */
+	
+	/*
+	 * repeated execution of same block-iteration 
+	 */
+	public static void main(String[] args) {
+		int count=0;
+		while(count<5) {
+			System.out.println("Hello World!!");
+			count++;
+		}
+	}
+}
