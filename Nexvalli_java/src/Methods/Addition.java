8297ff743e455d298fc445ff87f771c5f93c2e47
kiruba-code <talk2kiruba@gmail.com>
2021-11-04 08:13:07 +0530

    methods

--- Nexvalli_java/src/Methods/Addition.java
+++ Nexvalli_java/src/Methods/Addition.java
@@ -2,17 +2,33 @@ package Methods;
 
 public class Addition {
         //Declare two instance variables
-	int a=10;
-	int b=20;
+	static int a=10;
+	static int b=20;
+	int c=50;
+	int d=40;
 	//System.out.println(a+b)//it is  invlaid syntax because inside the class, we cannot write directly the business logic of the application
 	//Declaration of instance method
 	static void add( ){//no parameter 
 		//write the logic of adding two number and print it in the colsole
 		System.out.println(a+b);
 	}
-	public static void main(String[] args) {
-         //Addition a=new Addition();//object creation
-         add();//calling the method 
+	void sub() {
+		System.out.println(c-d);
 	}
+	public static void main(String[] args) {
+        Addition a=new Addition();//object creation
+        a.sub();
+       Addition.add();//calling the method 
+        //predefined methods
+        //user defined methods-instance methods and static methods
+       //main,retun type,call by value and call by reference
+	}
+	/*public --to cLL BY jvm from anywhere
+	 * static -- makes it class method so tat it can be called using class name without creatinf the object of the class
+	 * * 
+	 * void
+	 *  main
+	 */
+	
 
 }
